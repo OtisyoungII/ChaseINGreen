53daf6f8f091892651f482442efdbcaa6c7c645a
Otis YII <oyoung21@gmail.com>
2025-08-06 08:17:28 -0400

    Initial Commit

---
+++ ChaseINGreen/ChaseINGreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseINGreenApp.swift
+//  ChaseINGreen
+//
+//  Created by Otis Young on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ChaseINGreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
