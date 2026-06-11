65ff53a10687d8ba97c6680e3f78084a5bfe4c1b
Otis YII <oyoung21@gmail.com>
2026-06-11 17:02:57 -0400

    add pre trade context models

---
+++ ChaseINGreen/Models/PreTradeContext.swift
@@ -0,0 +1,97 @@
+//
+//  PreTradeContext.swift
+//  ChaseINGreen
+//
+//  Created by Otis Young on 6/11/26.
+//
+
+
+import Foundation
+
+struct PreTradeContextRequest: Codable {
+    let symbol: String
+    let direction: String?
+    let broker: String?
+    let accountType: String?
+    let accountSize: Double?
+    let plannedEntry: Double?
+    let plannedStopLoss: Double?
+    let plannedTakeProfit: Double?
+    let plannedSize: Double?
+
+    enum CodingKeys: String, CodingKey {
+        case symbol
+        case direction
+        case broker
+        case accountType = "account_type"
+        case accountSize = "account_size"
+        case plannedEntry = "planned_entry"
+        case plannedStopLoss = "planned_stop_loss"
+        case plannedTakeProfit = "planned_take_profit"
+        case plannedSize = "planned_size"
+    }
+}
+
+struct PreTradeContextResponse: Codable, Identifiable {
+    var id: String { symbol }
+
+    let symbol: String
+    let displaySymbol: String?
+
+    let canEnter: Bool
+    let entryGrade: Int
+    let setupBias: String
+    let setupQuality: String
+    let tradeTiming: String
+
+    let scenario: String?
+    let scenarioType: String?
+    let scenarioConfidence: Int?
+
+    let plainEnglishRead: String
+    let nextExpectedEvent: String?
+    let confirmation: String?
+    let invalidation: String?
+
+    let supportLevel: Double?
+    let resistanceLevel: Double?
+    let target1: Double?
+    let target2: Double?
+
+    let reasons: [String]
+    let warnings: [String]
+    let actions: [String]
+
+    let priceSource: String
+
+    enum CodingKeys: String, CodingKey {
+        case symbol
+        case displaySymbol = "display_symbol"
+
+        case canEnter = "can_enter"
+        case entryGrade = "entry_grade"
+        case setupBias = "setup_bias"
+        case setupQuality = "setup_quality"
+        case tradeTiming = "trade_timing"
+
+        case scenario
+        case scenarioType = "scenario_type"
+        case scenarioConfidence = "scenario_confidence"
+
+        case plainEnglishRead = "plain_english_read"
+        case nextExpectedEvent = "next_expected_event"
+        case confirmation
+        case invalidation
+
+        case supportLevel = "support_level"
+        case resistanceLevel = "resistance_level"
+        case target1 = "target_1"
+        case target2 = "target_2"
+
+        case reasons
+        case warnings
+        case actions
+
+        case priceSource = "price_source"
+    }
+}
